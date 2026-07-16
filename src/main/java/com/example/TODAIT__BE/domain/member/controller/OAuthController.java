17a26f4c3a1c975f9c63e51473258a5c6cf9fcab
chaeyounggggggg <olivia0827@naver.com>
2026-07-16 17:06:24 +0900

    feat: ígoogle 로그인 구현 완료

--- src/main/java/com/example/TODAIT__BE/domain/member/controller/OAuthController.java
+++ src/main/java/com/example/TODAIT__BE/domain/member/controller/OAuthController.java
@@ -5,7 +5,9 @@ import com.example.TODAIT__BE.domain.member.dto.response.OAuthLoginResponse;
 import com.example.TODAIT__BE.domain.member.service.OAuthService;
 import com.example.TODAIT__BE.global.apiPayload.ApiResponse;
 import com.example.TODAIT__BE.global.apiPayload.code.GeneralSuccessCode;
+import com.example.TODAIT__BE.infra.oauth.GoogleOAuthClient;
 import com.example.TODAIT__BE.infra.oauth.KakaoOAuthClient;
+import com.example.TODAIT__BE.infra.oauth.dto.GoogleUserInfo;
 import com.example.TODAIT__BE.infra.oauth.dto.KakaoUserInfo;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +20,7 @@ import org.springframework.web.bind.annotation.*;
 public class OAuthController {
 
     private final KakaoOAuthClient kakaoOAuthClient;
+    private final GoogleOAuthClient googleOAuthClient;
     private final OAuthService oAuthService;
 
     @PostMapping("/api/auth/kakao/login")
@@ -28,5 +31,22 @@ public class OAuthController {
         OAuthLoginResponse.OAuthLogin response = oAuthService.loginWithKakao(kakaoUserInfo);
 
         return ResponseEntity.ok(ApiResponse.onSuccess(GeneralSuccessCode.OK, response));
+    }
+
+
+    @PostMapping("/api/auth/google/login")
+    public ResponseEntity<ApiResponse<OAuthLoginResponse.OAuthLogin>> googleLogin(
+            @Valid @RequestBody OAuthLoginRequest.GoogleIdToken request
+    ){
+        GoogleUserInfo googleUserInfo = googleOAuthClient.verifyIdToken(request.idToken());
+        OAuthLoginResponse.OAuthLogin response = oAuthService.loginWithGoogle(googleUserInfo);
+
+        return ResponseEntity.ok(
+                ApiResponse.onSuccess(
+                    GeneralSuccessCode.OK,
+                    response
+                )
+
+        );
     }
 }
