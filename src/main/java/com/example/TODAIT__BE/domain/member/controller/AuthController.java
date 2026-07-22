b74e17e2736c52c4a63155dc17918b7abe0a67fc
chaeyounggggggg <olivia0827@naver.com>
2026-07-22 23:23:38 +0900

    feat: 읿반 회원가입 구현 완료

--- src/main/java/com/example/TODAIT__BE/domain/member/controller/AuthController.java
+++ src/main/java/com/example/TODAIT__BE/domain/member/controller/AuthController.java
@@ -1,4 +1,36 @@
 package com.example.TODAIT__BE.domain.member.controller;
 
-public class AuthController {
+import com.example.TODAIT__BE.domain.member.code.MemberSuccessCode;
+import com.example.TODAIT__BE.domain.member.controller.docs.AuthControllerDocs;
+import com.example.TODAIT__BE.domain.member.dto.request.SignRequest;
+import com.example.TODAIT__BE.domain.member.dto.response.AuthTokenResponse;
+import com.example.TODAIT__BE.domain.member.service.SignupService;
+import com.example.TODAIT__BE.global.apiPayload.ApiResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthController implements AuthControllerDocs {
+    private final SignupService signupService;
+
+    @PostMapping("/api/auth/signup")
+    @Override
+    public ResponseEntity<ApiResponse<AuthTokenResponse.Token>> signup(
+            @Valid @RequestBody SignRequest.SignUp request
+            ){
+                AuthTokenResponse.Token response = signupService.signup(request);
+
+                return ResponseEntity
+                        .status(HttpStatus.CREATED)
+                        .body(
+                                ApiResponse.onSuccess(MemberSuccessCode.SIGNUP_COMPLETED, response)
+                        );
+    }
+
 }
