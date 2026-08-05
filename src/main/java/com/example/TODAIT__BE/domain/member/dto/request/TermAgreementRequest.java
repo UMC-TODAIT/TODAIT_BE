57302bc08309c6b1c44ca68d3a69c4c4fef8df78
chaeyounggggggg <olivia0827@naver.com>
2026-08-05 22:42:58 +0900

    refactor: Swagger 요청 예시 및 필드 설명 추가

--- src/main/java/com/example/TODAIT__BE/domain/member/dto/request/TermAgreementRequest.java
+++ src/main/java/com/example/TODAIT__BE/domain/member/dto/request/TermAgreementRequest.java
@@ -1,12 +1,24 @@
 package com.example.TODAIT__BE.domain.member.dto.request;
 
 import com.example.TODAIT__BE.domain.member.enums.TermType;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
-
+@Schema(
+        name = "TermAgreementRequest",
+        description = "약관 동의 정보"
+)
 public record TermAgreementRequest(
+        @Schema(
+                description = "약관 유형",
+                example = "SERVICE"
+        )
         @NotNull
         TermType termType,
 
+        @Schema(
+                description = "약관 동의 여부",
+                example = "true"
+        )
         @NotNull
         Boolean agreed
 ) {
