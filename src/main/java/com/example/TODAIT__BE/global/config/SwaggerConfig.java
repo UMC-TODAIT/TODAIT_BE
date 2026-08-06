15ca487d0b4bbcd3c255e6a55153d1baf498e208
Jungsoo Ko <kojungsu1105@gmail.com>
2026-08-07 08:54:56 +0900

    refactor: Swagger 태그 설명 추가

--- src/main/java/com/example/TODAIT__BE/global/config/SwaggerConfig.java
+++ src/main/java/com/example/TODAIT__BE/global/config/SwaggerConfig.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.tags.Tag;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -30,6 +31,11 @@ public class SwaggerConfig {
         return new OpenAPI()
                 .info(info)
                 .addServersItem(new Server().url("/"))
+                .addTagsItem(new Tag().name("MEMBER").description("로그인, 회원가입, 이메일 인증, 온보딩, 회원 정보 API"))
+                .addTagsItem(new Tag().name("COURSE").description("임시 코스 및 저장 코스 API"))
+                .addTagsItem(new Tag().name("PLACE").description("장소 검색 및 상세 조회 API"))
+                .addTagsItem(new Tag().name("RECOMMENDATION").description("홈 추천 및 주변 핫플 추천 API"))
+                .addTagsItem(new Tag().name("TAXONOMY").description("장소 카테고리 등 기준 정보 API"))
                 .addSecurityItem(securityRequirement)
                 .components(components);
     }
